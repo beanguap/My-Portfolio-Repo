399be04b6da2bb99db6fb218a43b747adda1d411
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-04-30 12:12:14 -0400

    about

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const info = () => {
+const Info = () => {
   return (
-    <div>
-      
+    <div className='about__info grid'>
+      <div className="about__box">
+      <i class='bx bxs-award about__icon' ></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Entry Level</span>
+      </div>
+
+      <div className="about__box">
+        <i class='bx bx-briefcase-alt about__icon' ></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">15 Projects Built</span>
+      </div>
+
+      <div className="about__box">
+        <i class='bx bx-trending-up about__icon' ></i>
+
+        <h3 className="about__title">Commitment</h3>
+        <span className="about__subtitle">Continuos Growth, Passion, and Support</span>
+      </div>
     </div>
   )
 }
 
-export default info
+export default Info
