4e3d1a52ddd86ee9710ffb681d686c1106b40da6
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-08-08 20:03:30 -0400

    Projects/Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <Skills />
         <Contact />
+        <Footer />
       </main>
     </>
   );
